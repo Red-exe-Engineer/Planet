ab2b1973ffce141f81ee1a7f41940411391848b6
Red-exe-Engineer <90420989+Red-exe-Engineer@users.noreply.github.com>
2022-04-10 17:28:13 -0400

    Update mcpiedit.py
    Fix errors related to self.nbt and remove a lot of white space

--- planet/mcpiedit.py
+++ planet/mcpiedit.py
@@ -36,35 +36,34 @@ BOOLEAN_INTREGERS_REVERSED = {False: 0, True: 1}
 class AboutWindow(QWidget):
     def __init__(self):
         super().__init__()
-        
+
         layout = QVBoxLayout()
-        
+
         label = QLabel("About MCPIedit")
         label.setAlignment(Qt.AlignHCenter)
         font = label.font()  # Font used
         font.setPointSize(15)  # Set the font size
         label.setFont(font)  # Aplly the font onto the label
-        
+
         desc_label = QLabel("The default built-in NBT editor for Planet.\n\nMCPIedit makes use of Pi-NBT\n from the original MCPIedit project\nby TheBrokenRail, which is\nlicensed under the MIT license.")
         desc_label.setAlignment(Qt.AlignHCenter)
-        
+
         layout.addWidget(label)
         layout.addWidget(desc_label)
-        
-        self.setLayout(layout)
 
+        self.setLayout(layout)
 
 class FileSelectorTab(QWidget):
     def __init__(self):
         super().__init__()
 
         layout = QVBoxLayout()
-        
+
         info_label = QLabel("NBT editors allow you to edit your world\nfiles to change game modes, time,\nand even the world name. Select an NBT\nfile to edit using the button below.")
         info_label.setAlignment(Qt.AlignHCenter)
 
         self.load_button = QPushButton("Select NBT File")
-        
+
         self.about_button = QPushButton("About")
         self.about_button.clicked.connect(self.about_window)
 
@@ -73,11 +72,10 @@ class FileSelectorTab(QWidget):
         layout.addWidget(self.about_button)
 
         self.setLayout(layout)
-        
+
     def about_window(self):
         self.window = AboutWindow()
         self.window.show()
-
 
 class EditorTab(QWidget):
     def __init__(self, filename):
@@ -96,17 +94,16 @@ class EditorTab(QWidget):
         self.tabs.addTab(self.main_tab(), "General")
         self.tabs.addTab(self.world_tab(), "World")
 
-        self.name_edit.setText(str(self.nbt["LevelName"]))
-        self.timestamp_box.setValue(int(self.nbt["LastPlayed"]))
-        self.game_box.setCurrentText(GAME_INTREGERS[str(int(self.nbt["GameType"]))])
-        self.seed_edit.setText(str(int(self.nbt["RandomSeed"])))
-        self.time_edit.setText(str(int(self.nbt["Time"])))
+        self.name_edit.setText(str(self.nbt[""]["LevelName"]))
+        self.timestamp_box.setValue(int(self.nbt[""]["LastPlayed"]))
+        self.game_box.setCurrentText(GAME_INTREGERS[str(int(self.nbt[""]["GameType"]))])
+        self.seed_edit.setText(str(int(self.nbt[""]["RandomSeed"])))
+        self.time_edit.setText(str(int(self.nbt[""]["Time"])))
         #self.mobs_toggle.setChecked(BOOLEAN_INTREGERS[int(self.nbt["SpawnMobs"])]) # REMOVED BECAUSE DOES NOT WORK 
-        
-        
-        self.spawn_x_box.setValue(int(self.nbt["SpawnX"]))
-        self.spawn_y_box.setValue(int(self.nbt["SpawnY"]))
-        self.spawn_z_box.setValue(int(self.nbt["SpawnZ"]))
+
+        self.spawn_x_box.setValue(int(self.nbt[""]["SpawnX"]))
+        self.spawn_y_box.setValue(int(self.nbt[""]["SpawnY"]))
+        self.spawn_z_box.setValue(int(self.nbt[""]["SpawnZ"]))
 
         layout.addWidget(self.tabs)
 
@@ -118,7 +115,7 @@ class EditorTab(QWidget):
 
         layout = QGridLayout()
 
-        self.name_label = QLabel("World name")
+        self.name_label = QLabel("World Name")
 
         self.name_edit = QLineEdit()
         self.name_edit.setPlaceholderText("OneChunk")
@@ -134,22 +131,22 @@ class EditorTab(QWidget):
         self.timestamp_box = QSpinBox()
         self.timestamp_box.setMaximum(2147483647)
 
-        self.game_label = QLabel("Game mode")
+        self.game_label = QLabel("Game Mode")
 
         self.game_box = QComboBox()
         self.game_box.addItems(["Survival", "Creative"])
-        
+
         #self.mobs_toggle = AnimatedToggle(
         #    checked_color="#59b8e0",
         #    pulse_checked_color="#92cee8"
         #)
-        
+
         self.time_label = QLabel("Time (In Ticks)")
-        
+
         self.time_edit = QLineEdit()
         self.time_edit.setPlaceholderText("1770")
         self.time_edit.setValidator(QIntValidator())
-        
+
         self.back_button = QPushButton("Back")
 
         self.save_button = QPushButton("Save")
@@ -166,7 +163,7 @@ class EditorTab(QWidget):
 
         layout.addWidget(self.game_label, 3, 0)
         layout.addWidget(self.game_box, 3, 1)
-        
+
         layout.addWidget(self.time_label,  4, 0)
         layout.addWidget(self.time_edit,  4, 1)
 
@@ -176,64 +173,54 @@ class EditorTab(QWidget):
         widget.setLayout(layout)
 
         return widget
-        
-        
-    
+
     def world_tab(self):
-        
+ 
         layout = QGridLayout()
-        
+
         x_label = QLabel("Spawnpoint X")
-        
+
         self.spawn_x_box = QSpinBox()
         self.spawn_x_box.setMinimum(-128)
         self.spawn_x_box.setMaximum(128)
-        
+
         y_label = QLabel("Spawnpoint Y")
-        
+
         self.spawn_y_box = QSpinBox()
         self.spawn_y_box.setMinimum(-64)
         self.spawn_y_box.setMaximum(64)
 
-        
         z_label = QLabel("Spawnpoint Z")
-        
+
         self.spawn_z_box = QSpinBox()
         self.spawn_z_box.setMinimum(-128)
         self.spawn_z_box.setMaximum(128)
-        
+
         layout.addWidget(x_label,  0, 0)
         layout.addWidget(y_label,  1, 0)
         layout.addWidget(z_label,  2, 0)
 
-        
         layout.addWidget(self.spawn_x_box,  0, 1)
         layout.addWidget(self.spawn_y_box,  1, 1)
         layout.addWidget(self.spawn_z_box,  2, 1)
 
-        
-        
         widget = QWidget()
         widget.setLayout(layout)
-        
+
         return widget
 
     def save(self):
-        self.nbt["LevelName"] = nbt.nbtlib.String(self.name_edit.text())
-        self.nbt["LastPlayed"] = nbt.nbtlib.Long(self.timestamp_box.value())
-        self.nbt["GameType"] = GAME_TYPES[self.game_box.currentText()]
-        self.nbt["RandomSeed"] = nbt.nbtlib.Long(int(self.seed_edit.text()))
-        self.nbt["Time"] = nbt.nbtlib.Long(int(self.time_edit.text()))
-        
-        self.nbt["SpawnX"] = nbt.nbtlib.Int(self.spawn_x_box.value())
-        self.nbt["SpawnY"] = nbt.nbtlib.Int(self.spawn_y_box.value())
-        self.nbt["SpawnZ"] = nbt.nbtlib.Int(self.spawn_z_box.value())
-        
-        
-        
+        self.nbt[""]["LevelName"] = nbt.nbtlib.String(self.name_edit.text())
+        self.nbt[""]["LastPlayed"] = nbt.nbtlib.Long(self.timestamp_box.value())
+        self.nbt[""]["GameType"] = GAME_TYPES[self.game_box.currentText()]
+        self.nbt[""]["RandomSeed"] = nbt.nbtlib.Long(int(self.seed_edit.text()))
+        self.nbt[""]["Time"] = nbt.nbtlib.Long(int(self.time_edit.text()))
+
+        self.nbt[""]["SpawnX"] = nbt.nbtlib.Int(self.spawn_x_box.value())
+        self.nbt[""]["SpawnY"] = nbt.nbtlib.Int(self.spawn_y_box.value())
+        self.nbt[""]["SpawnZ"] = nbt.nbtlib.Int(self.spawn_z_box.value())
 
         nbt.save_nbt(self.nbt, self.filename)
-
 
 class NBTEditor(QWidget):
     def __init__(self):
@@ -256,17 +243,16 @@ class NBTEditor(QWidget):
             f"/home/{USER}/.minecraft-pi/games/com.mojang/minecraftWorlds/",
             "Minecraft Pi Level NBT (level.dat)",
         )
-        
+
         if fname[0] == '':
             return
-            
+
         editor = EditorTab(fname[0])
         editor.back_button.clicked.connect(lambda: self.layout.setCurrentIndex(0))
 
         self.layout.insertWidget(1,  editor)
         self.layout.setCurrentIndex(1)
         self.setLayout(self.layout)
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
